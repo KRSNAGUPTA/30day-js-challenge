9448ceef61abdfbb7a1908947884da577c2e64a0
KRISHNA GUPTA <121710869+KRSNAGUPTA@users.noreply.github.com>
2024-07-14 00:11:47 +0530

    Task Done

--- day1.js
+++ day1.js
@@ -1,1 +1,34 @@
-console.log("task1");
+// Activity 1 : Variable Declaration
+// task 1
+// var age = 20;
+// console.log(age);
+
+// Task 2
+// let name = "Krishna Gupta";
+// console.log(name);
+
+// Activity 2 : Constant Declaration
+// const newConst = 1;
+// // newConst = true; //will give error
+// console.log(newConst);
+
+// Activity 3 : Data Types
+// /*number*/ let num = 1;
+// console.log(typeof(num));
+// /*string*/ let str = "1";
+// console.log(typeof(str));
+// /*boolean*/ let bool = 1;
+// console.log(typeof(bool));
+// /*array*/ let arr = [1,2,3];
+// console.log(typeof(arr));
+
+// Activity 4 : Reassigning Variables
+// let num = 5;
+// console.log(num);
+// num = 6;
+// console.log(num);
+
+// // Activity 5 : Understanding const
+// const num = 5;
+// num = 6 //Error num = 6 TypeError: Assignment to constant variable.
+// console.log(num);
